0ea405517a24ed208c0f98828738a5e3f4f39b93
Tiago Gomes <tacg@tacgomes.com>
2020-10-20 17:05:50 +0100

    Support connectivity drops
    Support connectivity drops without having to upload the whole file.

--- filereceiver/filereceiver.rs
+++ filereceiver/filereceiver.rs
@@ -1,10 +1,14 @@
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+
+const BUF_SIZE : usize = 1024;
+const MAX_BYTES_NOT_ACKNOWLEDGED : u64 = 1 * 1024 * 1024;
 
 pub struct FileReceiver {
     port: u16
 }
-
 
 impl FileReceiver {
     pub fn new(port: u16) -> FileReceiver {
@@ -22,38 +26,59 @@ impl FileReceiver {
     }
 
     fn handle_connection(mut stream: TcpStream) {
-        let mut data = [0 as u8; 1];
-        stream.read_exact(&mut data).expect("Failed to read filename length");
-        let filename_length = u8::from_be_bytes(data);
+        let mut u8_buf = [0 as u8; 1];
+        let mut u64_buf = [0 as u8; 8];
 
-        let mut filename_bytes = vec![0 as u8; filename_length as usize];
-        stream.read_exact(&mut filename_bytes).expect("Failed to read filename bytes");
-        let filename = String::from_utf8(filename_bytes)
+        stream.read_exact(&mut u8_buf).expect("Failed to read filename length");
+        let filename_len = u8::from_be_bytes(u8_buf);
+
+        let mut filename_buf = vec![0 as u8; filename_len as usize];
+        stream.read_exact(&mut filename_buf).expect("Failed to read filename");
+        let filename = String::from_utf8(filename_buf)
             .expect("Failed to construct filename string");
 
-        let mut filesize_data = [0 as u8; 8];
-        stream.read_exact(&mut filesize_data).expect("Failed to read file size");
-        let _filesize = u64::from_be_bytes(filesize_data);
+        stream.read_exact(&mut u64_buf).expect("Failed to read file size");
+        let file_size = u64::from_be_bytes(u64_buf);
 
-        let path = std::path::Path::new(&filename);
+        stream.read_exact(&mut u64_buf).expect("Failed to read file offset");
+        let offset = u64::from_be_bytes(u64_buf);
 
-        let mut file = std::fs::File::create(path.file_name().unwrap())
+        let mut file = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(Path::new(&filename).file_name().unwrap())
             .expect("Failed to open file");
 
+        file.seek(SeekFrom::Start(offset)).expect("Failed to seek");
+
+        let mut bytes_received : u64 = 0;
+        let mut bytes_not_acknowledged : u64 = 0;
+        let mut buf = [0 as u8; BUF_SIZE];
+
         loop {
-            let mut data = [0 as u8; 50];
-            match stream.read(&mut data) {
+            match stream.read(&mut buf) {
                 Ok(size) => {
                     if size == 0 {
                         println!("File transfer completed");
                         break;
                     } else {
-                        file.write_all(&data[..size]).expect("Failed to write to file");
+                        file.write_all(&buf[..size]).expect("Failed to write to file");
+
+                        bytes_received += size as u64;
+                        bytes_not_acknowledged += size as u64;
+
+                        if bytes_not_acknowledged >= MAX_BYTES_NOT_ACKNOWLEDGED
+                                || (offset + bytes_received) == file_size {
+                            file.flush().expect("Failed to flush the file");
+                            match stream.write(&(bytes_received + offset).to_be_bytes()) {
+                                Err(err) => eprintln!("WARNING: failed to send acknowledgedment: {}", err),
+                                Ok(_) => bytes_not_acknowledged = 0,
+                            }
+                        }
                     }
                 },
                 Err(err) => eprintln!("Error reading data: {}", err),
             }
         }
     }
-
 }
--- fileuploader/fileuploader.rs
+++ fileuploader/fileuploader.rs
@@ -1,6 +1,12 @@
 use std::io::prelude::*;
-use std::fs::File;
+use std::io::ErrorKind;
+use std::io::SeekFrom;
+use std::fs::{File, metadata};
 use std::net::TcpStream;
+use std::thread;
+use std::time;
+
+const BUF_SIZE : usize = 1024;
 
 pub struct FileUploader {
     host: String,
@@ -13,25 +19,93 @@ impl FileUploader {
     }
 
     pub fn upload(&self, filename: String) {
-        let addr = format!("{}:{}", self.host, self.port);
-        let mut stream = TcpStream::connect(&addr).expect("Failed to connect");
+        let mut stream = self.connect();
 
-        FileUploader::send_header(&mut stream, &filename);
+        let mut bytes_acknowledged : u64 = 0;
 
-        let mut f = File::open(&filename).expect("Failed to open the file");
-        let mut buffer = [0; 1024];
+        let file_size = metadata(&filename).expect("Failed to read file size").len();
+
+        self.send_header(&mut stream, &filename, file_size, 0);
+
+        let mut file = File::open(&filename).expect("Failed to open the file");
+
+        let mut buf = [0 as u8; BUF_SIZE];
+        let mut u64_buf = [0 as u8; 8];
+
         loop {
-            let n = f.read(&mut buffer[..]).expect("Failed to read");
-            stream.write(&buffer[..n]).expect("Failed to write to socket");
-            if n == 0 {
-                break;
+            stream.set_nonblocking(true).expect("set_nonblocking call failed");
+            match stream.read_exact(&mut u64_buf) {
+                Ok(_) => {
+                    bytes_acknowledged = u64::from_be_bytes(u64_buf);
+                },
+                Err(err) => {
+                    match err.kind() {
+                        ErrorKind::WouldBlock => {},
+                        _ => eprintln!("WARNING: failed to read acknowledgement: {}", err)
+                    }
+                }
+            }
+
+            // FIXME: fix this blocking/not-blocking madness
+            stream.set_nonblocking(false).expect("set_nonblocking call failed");
+
+            let n = file.read(&mut buf[..]).expect("Failed to read");
+            match stream.write(&buf[..n]) {
+                Ok(size) => {
+                    if size == 0 {
+                        while file_size != bytes_acknowledged {
+                            match stream.read_exact(&mut u64_buf) {
+                                Ok(_) => bytes_acknowledged = u64::from_be_bytes(u64_buf),
+                                Err(err) => eprintln!("WARNING: failed to read acknowledgement: {}", err)
+                            }
+                        }
+                        break;
+                    }
+                },
+                Err(e) => {
+                    match e.kind() {
+                        ErrorKind::ConnectionReset|ErrorKind::BrokenPipe => {
+                            eprintln!("Connection reset");
+                            stream = self.connect();
+                            file.seek(SeekFrom::Start(bytes_acknowledged)).expect("Failed to seek");
+                            self.send_header(
+                                &mut stream, &filename, file_size, bytes_acknowledged);
+                        },
+                        _ => {
+                            panic!("Unhandled error: {}", e);
+                        },
+                    }
+                },
             }
         }
     }
 
-    fn send_header(stream: &mut TcpStream, filename: &str) {
-        let meta = std::fs::metadata(&filename)
-            .expect("Failed to read file size");
+    fn connect(&self) -> TcpStream {
+        let addr = format!("{}:{}", self.host, self.port);
+        let mut stream;
+
+        loop {
+            stream = TcpStream::connect(&addr);
+            match stream {
+                Err(err) =>
+                    match err.kind() {
+                        ErrorKind::ConnectionRefused => {
+                            eprintln!("Connection refused. Retrying...");
+                            thread::sleep(time::Duration::from_secs(1));
+                        },
+                        _ => panic!("Unhandled error: {}", err),
+                    },
+                Ok(_) => break,
+            }
+        }
+
+        eprintln!("Connection established");
+        stream.unwrap()
+    }
+
+    fn send_header(
+            &self, stream: &mut TcpStream,
+            filename: &str, file_size: u64, file_offset: u64) {
 
         stream.write(&(filename.len() as u8).to_be_bytes())
             .expect("Failed to send filename length");
@@ -39,7 +113,10 @@ impl FileUploader {
         stream.write(filename.as_bytes())
             .expect("Failed to send filename");
 
-        stream.write(&meta.len().to_be_bytes())
+        stream.write(&file_size.to_be_bytes())
             .expect("Failed to send file size");
+
+        stream.write(&file_offset.to_be_bytes())
+            .expect("Failed to send file offset");
     }
 }
