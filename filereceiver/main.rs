9098626178ad4f4619a4042d4540f505a66d843f
Tiago Gomes <tacg@tacgomes.com>
2020-10-20 17:05:46 +0100

    Add command line parsing

--- filereceiver/main.rs
+++ filereceiver/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+
+fn show_usage(progname: &str) {
+    eprintln!(
+"Usage: {} [Options...] PORT
+
+Options:
+\t-h,--help\t\tShow usage",
+        progname
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut port : u16 = 0;
+
+    let mut i = 1;
+    let args : Vec<String> = env::args().collect();
+
+    while i < args.len() {
+        match args[i].as_str() {
+            "--help" | "-h" => {
+                show_usage(&args[0]);
+                std::process::exit(0);
+            },
+            _ => port = args[i].parse::<u16>().unwrap()
+
+        }
+        i += 1;
+    }
+
+    if port == 0 {
+        eprintln!("Error: missing arguments\n");
+        show_usage(&args[0]);
+        std::process::exit(1);
+    }
 }
--- fileuploader/main.rs
+++ fileuploader/main.rs
@@ -1,3 +1,53 @@
+use std::env;
+
+fn show_usage(progname: &str) {
+    eprintln!(
+"Usage: {} [Options...] FILE
+
+Options:
+\t--host HOST\t\tSpecify the server IP address
+\t--port HOST\t\tSpecify the server port
+\t-h,--help\t\tShow usage",
+        progname
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut host = String::new();
+    let mut port : u16 = 0;
+    let mut filename = String::new();
+
+    let mut i = 1;
+    let args : Vec<String> = env::args().collect();
+
+    while i < args.len() {
+        match args[i].as_str() {
+            "--host" => {
+                if i + 1 >= args.len() {
+                    show_usage(&args[0]);
+                }
+                host = args[i + 1].clone();
+                i += 1;
+            },
+            "--port" => {
+                if i + 1 >= args.len() {
+                    show_usage(&args[0]);
+                }
+                port = args[i + 1].parse::<u16>().unwrap();
+                i += 1;
+            }
+            "--help" | "-h" => {
+                show_usage(&args[0]);
+                std::process::exit(0);
+            },
+            _ => filename = args[i].clone()
+        }
+        i += 1;
+    }
+
+    if host.is_empty() || port == 0 || filename.is_empty() {
+        eprintln!("Error: missing arguments\n");
+        show_usage(&args[0]);
+        std::process::exit(1);
+    }
 }
